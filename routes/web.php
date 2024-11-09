a7c820205bda1ce378a722e4b5d734a75001a26a
Dedy Putra Aditia <73837147+dedyputra@users.noreply.github.com>
2024-11-09 11:14:01 +0700

    update route clean code

--- routes/web.php
+++ routes/web.php
@@ -2,46 +2,44 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\loginController;
+use App\Http\Controllers\LoginController;
 use App\Models\Employee;
-use Database\Seeders\EmployeeSeeder;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Here is where you can register web routes for your application.
+| These routes are loaded by the RouteServiceProvider within a group
+| which contains the "web" middleware group. Now create something great!
 |
 */
 
+// Landing Page
 Route::get('/', function () {
-  $jumlahpegawai = Employee::count();
-  $jumlahpegawaicowok = Employee::where('jeniskelamin', 'cowok')->count();
-  $jumlahpegawaicewek = Employee::where('jeniskelamin', 'cewek')->count();
+  $jumlahPegawai = Employee::count();
+  $jumlahPegawaiCowok = Employee::where('jeniskelamin', 'cowok')->count();
+  $jumlahPegawaiCewek = Employee::where('jeniskelamin', 'cewek')->count();
 
-  return view('welcome', compact('jumlahpegawai', 'jumlahpegawaicowok', 'jumlahpegawaicewek'));
+  return view('welcome', compact('jumlahPegawai', 'jumlahPegawaiCowok', 'jumlahPegawaiCewek'));
 })->middleware('auth');
 
-Route::get('/pegawai', [EmployeeController::class, 'index'])->name('pegawai')->middleware('auth');
+// Auth Routes
+Route::controller(LoginController::class)->group(function () {
+  Route::get('/login', 'login')->name('login');
+  Route::post('/loginproses', 'loginproses')->name('loginproses');
+  Route::get('/register', 'register')->name('register');
+  Route::post('/registeruser', 'registeruser')->name('registeruser');
+  Route::get('/logout', 'logout')->name('logout');
+});
 
-Route::get('/tambahpegawai', [EmployeeController::class, 'tambahpegawai'])->name('tambahpegawai')->middleware('auth');
-
-Route::post('/insertdata', [EmployeeController::class, 'insertdata'])->name('insertdata');
-
-Route::get('/tampilkandata/{id}', [EmployeeController::class, 'tampilkandata'])->name('tampilkandata');
-
-Route::post('/updatedata/{id}', [EmployeeController::class, 'updatedata'])->name('updatedata');
-
-Route::get('/delete/{id}', [EmployeeController::class, 'delete'])->name('delete');
-// Route::get('/editfoto/{id}', [EmployeeController::class, 'editfoto'])->name('foto');
-
-Route::get('/login', [loginController::class, 'login'])->name('login');
-Route::post('/loginproses', [loginController::class, 'loginproses'])->name('loginproses');
-
-Route::get('/register', [loginController::class, 'register'])->name('register');
-Route::post('/registeruser', [loginController::class, 'registeruser'])->name('registeruser');
-
-Route::get('/logout', [loginController::class, 'logout'])->name('logout');
+// Employee Routes (Protected by Auth Middleware)
+Route::middleware('auth')->prefix('pegawai')->name('pegawai.')->group(function () {
+  Route::get('/', [EmployeeController::class, 'index'])->name('index');
+  Route::get('/tambah', [EmployeeController::class, 'tambahpegawai'])->name('tambah');
+  Route::post('/insert', [EmployeeController::class, 'insertdata'])->name('insert');
+  Route::get('/edit/{id}', [EmployeeController::class, 'tampilkandata'])->name('edit');
+  Route::post('/update/{id}', [EmployeeController::class, 'updatedata'])->name('update');
+  Route::get('/delete/{id}', [EmployeeController::class, 'delete'])->name('delete');
+});
